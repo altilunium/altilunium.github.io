856a4af768a7d956319c7f755bee1ccdf2caa317
Altilunium <70379302+altilunium@users.noreply.github.com>
2026-09-23 23:02:49 +0700

    Add icons to service worker asset list

--- util/d/sw.js
+++ util/d/sw.js
@@ -4,7 +4,9 @@ const ASSETS = [
     "./",
     "./index.html",
     "./manifest.json",
-    "./tailwind.js"
+    "./tailwind.js",
+    "./icon-192.png",
+    "./icon-512.png",
 ];
 
 self.addEventListener("install", event => {

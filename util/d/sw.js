afae362b26b840447153c3334ca243d5a75496d4
Altilunium <70379302+altilunium@users.noreply.github.com>
2026-09-23 22:53:47 +0700

    Create sw.js

---
+++ util/d/sw.js
@@ -0,0 +1,22 @@
+const CACHE = "c-v2";
+
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./tailwind.js"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(res => {
+            return res || fetch(event.request);
+        })
+    );
+});

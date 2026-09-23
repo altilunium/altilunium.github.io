2bd08f44420f0ad1db3e398fe0dc933d715dbd02
Altilunium <70379302+altilunium@users.noreply.github.com>
2026-09-23 22:24:45 +0700

    Update sw.js

--- util/c/sw.js
+++ util/c/sw.js
@@ -1,8 +1,21 @@
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', (e) => {
-    if (e.request.url.includes('wikidata.org')) return;
-    e.respondWith(
-        caches.match(e.request).then((res) => res || fetch(e.request).catch(() => new Response('Offline')))
+const CACHE = "c-v1";
+
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(res => {
+            return res || fetch(event.request);
+        })
     );
 });
